ecd84d49909666f1eabd608648a6e342666a4dfa
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-10-16 10:35:50 -0500

    Course

--- src/AppBundle/Controller/CoursesController.php
+++ src/AppBundle/Controller/CoursesController.php
@@ -182,4 +182,218 @@ class CoursesController extends Controller
         $dompdf->render();
         $dompdf->stream($courseUser->getCourses()->getCourse().".pdf");
     }
+
+    public function copyAction($course)
+    {
+        /**
+         * Create Course
+         */
+        $coursesManager = $this->get('moocsy.courses_manager');
+        $course = $coursesManager->findOneBySlug($course);
+
+        $newCourse = $coursesManager->create();
+
+        //$quantityCourses = count($coursesManager->findAll()) + 1;
+
+        $newCourse->setCourse($course->getCourse());
+        $newCourse->setDescription($course->getDescription());
+        $newCourse->setTemporality($course->getTemporality());
+        $newCourse->setEnabled($course->getEnabled());
+        $newCourse->setPublished($course->getPublished());
+        $newCourse->setSKU($course->getSKU());
+        $newCourse->setPath($course->getPath());
+
+        /**
+         * Persist Course
+         */
+
+        $coursesManager->save($newCourse);
+
+        if($course->getCoursesCovers()){
+
+            /**
+             * Create CourseCovers
+             */
+
+            $coursesCoversManager = $this->get('moocsy.courses_covers_manager');
+            $coursesCovers = $coursesCoversManager->create();
+
+            $coursesCovers->setCourses($newCourse);
+            $coursesCovers->setPath($course->getCoursesCovers()->getPath());
+
+            /**
+             * Persist CourseCovers
+             */
+
+            $coursesCoversManager->save($coursesCovers);
+        }
+
+        if($course->getAttachments()){
+
+            foreach($course->getAttachments() as $attachment){
+
+                /**
+                 * Create CourseAttachments
+                 */
+
+                $coursesAttachmentsManager = $this->get('moocsy.courses_attachments_manager');
+                $coursesAttachments = $coursesAttachmentsManager->create();
+
+                $coursesAttachments->setCourses($newCourse);
+                $coursesAttachments->setAttachment($attachment->getAttachment());
+                $coursesAttachments->setPath($attachment->getPath());
+
+                /**
+                 * Persist CourseCovers
+                 */
+
+                $coursesAttachmentsManager->save($coursesAttachments);
+            }
+
+        }
+
+        /**
+         * Create Course Modules
+         */
+
+        foreach($course->getModules() as $module){
+
+            $modulesManager = $this->get('moocsy.modules_manager');
+            $modules = $modulesManager->create();
+
+            $modules->setCourses($newCourse);
+            $modules->setModule($module->getModule());
+            $modules->setDescription($module->getDescription());
+
+            $modulesManager->save($modules);
+
+            foreach($module->getItems() as $item){
+
+                $itemsManager = $this->get('moocsy.items_manager');
+                $items = $itemsManager->create();
+
+                $items->setModules($modules);
+                $items->setItem($item->getItem());
+                $items->setItemsType($item->getItemsType());
+                $items->setDescription($item->getDescription());
+
+
+
+                $itemsManager->save($items);
+
+
+                switch ($item->getItemsType()) {
+                    case 'items_audio':
+
+                        if($item->getItemsAudio()){
+                            $itemsAudioManager = $this->get('moocsy.items_audio_manager');
+
+                            $itemsAudio = $itemsAudioManager->create();
+
+                            $itemsAudio->setItems($items);
+                            $itemsAudio->setAudio($item->getItemsAudio()->getAudio());
+
+                            $itemsAudioManager->save($itemsAudio);
+                        }
+
+                        break;
+                    case 'items_audio_down':
+
+                        if($item->getItemsAudioDown()){
+
+                            $itemsAudioDownManager = $this->get('moocsy.items_audio_down_manager');
+
+                            $itemsAudioDown = $itemsAudioDownManager->create();
+
+                            $itemsAudioDown->setItems($items);
+                            $itemsAudioDown->setAudio($item->getItemsAudioDown()->getAudio());
+
+                            $itemsAudioDownManager->save($itemsAudioDown);
+                        }
+
+                        break;
+                    case 'items_file':
+
+                        if($item->getItemsFile()){
+
+                            $itemsFileManager = $this->get('moocsy.items_file_manager');
+
+                            $itemsFile = $itemsFileManager->create();
+
+                            $itemsFile->setItems($items);
+                            $itemsFile->setPath($item->getItemsFile()->getPath());
+
+                            $itemsFileManager->save($itemsFile);
+                        }
+
+                        break;
+                    case 'items_forum':
+
+                        if($item->getItemsForum()){
+
+                            $itemsForumManager = $this->get('moocsy.items_forum_manager');
+
+                            $itemsForum = $itemsForumManager->create();
+
+                            $itemsForum->setItems($items);
+                            $itemsForum->setForum($item->getItemsForum()->getForum());
+
+                            $itemsForumManager->save($itemsForum);
+                        }
+
+                        break;
+                    case 'items_video':
+
+                        if($item->getItemsVideo()){
+
+                            $itemsVideoManager = $this->get('moocsy.items_video_manager');
+
+                            $itemsVideo = $itemsVideoManager->create();
+
+                            $itemsVideo->setItems($items);
+                            $itemsVideo->setVideo($item->getItemsVideo()->getVideo());
+
+                            $itemsVideoManager->save($itemsVideo);
+                        }
+
+                        break;
+                    case 'items_quiz':
+
+                        if($item->getItemsQuiz()){
+
+                            $itemsQuizManager = $this->get('moocsy.items_quiz_manager');
+
+                            $itemsQuiz = $itemsQuizManager->create();
+
+                            $itemsQuiz->setItems($items);
+                            $itemsQuiz->setQuiz($item->getItemsQuiz()->getQuiz());
+
+                            $itemsQuizManager->save($itemsQuiz);
+
+                            if($item->getItemsQuiz()->getQuestions()){
+
+                                foreach($item->getItemsQuiz()->getQuestions() as $question){
+
+                                    $questionsManager = $this->get('moocsy.questions_manager');
+                                    $questions = $questionsManager->create();
+
+                                    $questions->setItemsQuiz($itemsQuiz);
+                                    $questions->setQuestion($question->getQuestion());
+
+                                    $questionsManager->save($questions);
+                                }
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+        }
+
+
+        return $this->redirect($this->generateUrl('moocsy_admin_courses'));
+    }
 }
